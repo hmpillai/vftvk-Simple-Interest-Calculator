189cad67851939a9dac2577e12b5dda2ef650326
hmpillai <hmpillai@gmail.com>
2022-01-04 16:16:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
+{    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    // raise warning if principal is less than or equal to 0
+    if (parseInt(principal)<=0){
+        alert("Enter a positive number")
+        document.getElementById("principal").focus
+    }
+    // calclate interest and display message
+    else{
+        var interest = parseInt(principal) * parseInt(years) * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>";  
+    }  
 }
-        
+
+function updateRate() 
+{
+    // update the slider value when slider changes
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
